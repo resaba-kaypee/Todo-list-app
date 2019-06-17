dd4af2f2f989b0d24e61a71d3deb9290f1aa8e4d
kaypee <resaba.kaypee@gmail.com>
2019-06-18 04:01:37 +0700

    completed a todo app

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,24 +1,48 @@
 let bodyParser = require('body-parser');
+let mongoose = require('mongoose');
 
-let data = [{item: 'buy mewk'}, {item: 'eat lunch'}, {item: 'beat jhonny'}];
+// Connect to the database
+
+mongoose.connect('mongodb+srv://test:test@cluster0-lfprl.gcp.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true }, err => {
+  if (err) {
+    console.log('error: ' + err);
+  } else {
+    console.log('connected to db')
+  }
+})
+
+// Create a schema - blueprint
+let todoSchema = new mongoose.Schema({
+  item: String
+});
+
+let Todo = mongoose.model('Todo', todoSchema);
 
 let urlencodedParser = bodyParser.urlencoded( {extended: false} )
 
 module.exports = function(app) {
 
   app.get('/todo', function (req, res) {
-    res.render('todo', {todos: data});
+    // get data from db and pass to the view
+    Todo.find({}, (err, data) => {
+      if (err) throw err;
+      res.render('todo', {todos: data});
+    })
   })
 
   app.post('/todo', urlencodedParser, function (req, res) {
-    data.push(req.body);
-    res.json(data);
+    // get data from view then add to db
+    let newTodo = Todo(req.body).save((err, data) => {
+      if (err) throw err;
+      res.json(data);
+    })
   })
 
   app.delete('/todo/:item', function (req, res) {
-    data = data.filter(function(todo) {
-      return todo.item.replace(/ /g, '-') !== req.params.item;
-    });
-    res.json(data)
+    // delete the requested item from db
+    Todo.find({item: req.params.item.replace(/\-/g, " ")}).deleteOne((err, data) => {
+      if (err) throw err;
+      res.json(data);
+    })
   });
 }
